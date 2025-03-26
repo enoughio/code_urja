93c0784853914e9a9b871f8b636d4f8960ba69d2
kanak gupta <kanakgupta120804@gmail.com>
2025-03-26 23:38:12 +0530

    adding data

---
+++ frontend/app/home/page.jsx
@@ -0,0 +1,19 @@
+import Hero from "@/componets/Hero"
+import WhyChoseUsero from "@/componets/WhyChoseUs.jsx"
+// import FAQ from "@/componets/FAQ.jsx"
+
+
+const page = () => {
+  return (
+    <div>
+        <Hero />
+        <WhyChoseUsero />
+        {/* <FAQ /> */}
+
+
+
+    </div>
+  )
+}
+
+export default page
